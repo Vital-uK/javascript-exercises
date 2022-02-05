379a6fe67c4d3d1971d6b15f09e205b93d77fac8
Vital-uK <kolmykovvn@gmail.com>
2022-02-06 00:04:18 +0300

    3 exercise finished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let resultString = "";
+    /*for (let i = string.length - 1; i >= 0; i--) {
+        resultString += string.slice(i,i+1);
+    }
+    return resultString;*/
+    resultString = string.split("").reverse().join("");
+    return resultString;
 
 };
 
