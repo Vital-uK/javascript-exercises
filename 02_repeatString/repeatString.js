752dba4a33d651896a3f05b80e6250f6e3999924
Vital-uK <kolmykovvn@gmail.com>
2022-02-05 23:33:44 +0300

    2 exercise finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string,repeatNum) {
+    if (repeatNum >= 0) {
+        resultString = "";
+        for (let i = 0; i < repeatNum; i++) {
+            resultString += string;
+        }
+    } else {
+        resultString = "ERROR"
+    }
+    return resultString;
 };
 
 // Do not edit below this line
