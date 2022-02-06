c060a5f5c8d25e95144f86600825781bb8e0e149
Vital-uK <kolmykovvn@gmail.com>
2022-02-06 13:59:58 +0300

    4 exercise finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...others) {
+    let resultArray = [];
+    if (Array.isArray(array)) {
+        resultArray = array;
+        others.forEach(item => resultArray = resultArray.filter(arrayItem => arrayItem !== item));
+        /*for (let argument of others) {
+            resultArray = resultArray.filter(item => item !== argument); 
+        }*/
+    }
+    return resultArray;
 };
 
 // Do not edit below this line
