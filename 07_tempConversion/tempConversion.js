bde08d0859bfd9549c0e98bcb07210eb40971408
Vital-uK <kolmykovvn@gmail.com>
2022-02-06 15:47:21 +0300

    7 exercise finished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
+const ftoc = function(fTemp) {
+  if (Number.isFinite(fTemp)) {
+    return +((fTemp - 32)*5/9).toFixed(1);
+  }
 
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  if (Number.isFinite(cTemp)) {
+    return +(cTemp*9/5 + 32).toFixed(1);
+  }
 };
 
 // Do not edit below this line
