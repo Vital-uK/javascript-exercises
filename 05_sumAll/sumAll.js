68497c44b600cefa9834e41979930216d4089892
Vital-uK <kolmykovvn@gmail.com>
2022-02-06 14:42:47 +0300

    5 exercise finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    function positiveInt(num){
+        return Number.isInteger(num) && (num > 0);
+    }
+    if (positiveInt(num1) && positiveInt(num2)) {
+        let sum = 0;
+        let i = num1;
+        let max = num2;
+        if (num1 > num2) {
+            i = num2;
+            max = num1;
+        }
+        for (;i<=max; i++) sum +=i;
+        return sum;
+    } else {
+        return "ERROR"
+    }
 
 };
 
