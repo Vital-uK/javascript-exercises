49730c257cdf705585742138dda6ba6604742d7c
Vital-uK <kolmykovvn@gmail.com>
2022-02-12 01:51:08 +0300

    exercise 9 finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let array = [...str.toUpperCase()]
+    .filter(char => char != char.toLowerCase());
+    return array.join('') == array.reverse().join('');
 };
 
 // Do not edit below this line
