d34a20da38dcf7fdc9620495ad37b1c51739d3b7
Vital-uK <kolmykovvn@gmail.com>
2022-02-12 02:06:42 +0300

    exercise 10 finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    if (Number.isInteger(+a) && (+a > 0)){
+        let first = 1;
+        let second = 1;
+        let result = 1;
+        let i = 2;
+        while (i < a) {
+            result = first + second;
+            first = second;
+            second = result;
+            i++;
+        } return result;
 
+    } else return 'OOPS'
 };
 
 // Do not edit below this line
