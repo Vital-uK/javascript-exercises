f3d5b86a5e2184202a448e4e1ed9cf9ebd229595
Vital-uK <kolmykovvn@gmail.com>
2022-02-12 03:25:46 +0300

    exercise 12 finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const currentYear = (new Date()).getFullYear();
+    let result = array.reduce((oldest, person) => {  
+        let firstAge;
+        let secondAge;  
+        if (!('yearOfDeath' in oldest)) {
+            firstAge = currentYear - oldest.yearOfBirth;
+        } else {
+            firstAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        }
+        if (!('yearOfDeath' in person)) {
+            secondAge = currentYear - person.yearOfBirth;
+        } else {
+            secondAge = person.yearOfDeath - person.yearOfBirth;
+        }        
+        return (secondAge > firstAge) ? person : oldest;
+    } )
+    return result
 
 };
 
